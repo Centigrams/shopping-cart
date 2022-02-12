94ee4e5c67ec0af702355bc28db18b6d6fb7ab6b
Sidd Bairan <bairansa@gmail.com>
2022-02-12 16:21:09 +0800

    Setup routers

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './Home/Home';
+import Products from './Products/Products';
+import Contact from './Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
