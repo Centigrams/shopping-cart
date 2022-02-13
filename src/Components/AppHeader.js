c73bea2f1a1a474dc11b279dec7207a742af37d4
Sidd Bairan <bairansa@gmail.com>
2022-02-13 11:55:34 +0800

    Add link in app name

--- src/Components/AppHeader.js
+++ src/Components/AppHeader.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CustomAppBar from '../Styles/CustomComponents/CustomAppBar';
 import styles from '../Styles/AppHeader.module.css';
+import linkStyles from '../Styles/HeaderLinks.module.css';
 import HeaderLinks from './HeaderLinks';
 
 function AppHeader() {
   return (
     <CustomAppBar>
       <div>
-        <h1 className={styles.appName}>SampleStore</h1>
+        <Link to="/" className={linkStyles.links}>
+          <h1 className={styles.appName}>SampleStore</h1>
+        </Link>
       </div>
       <HeaderLinks />
     </CustomAppBar>
