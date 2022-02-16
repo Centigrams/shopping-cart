02e8fd91464a1d38438ecc26a539ce00a4f0420f
Sidd Bairan <bairansa@gmail.com>
2022-02-16 09:12:48 +0800

    Setup product page layout

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import styles from '../../Styles/Products.module.css';
 
+// TODO: Website layout (refer to Bose's website).
+// TODO: Product Images.
+// TODO: Product Data array.
+// TODO: Product Cards.
+// TODO: Configure Product Filter
 function Products() {
   return (
-    <div>
-      <h1>Products</h1>
+    <div className={styles.productsContainer}>
+      <section className={styles.photoSection}>
+        Placeholder
+      </section>
+      <section className={styles.productGroup}>
+        <div className={styles.product}>
+          over ear headphones
+        </div>
+        <div className={styles.product}>
+          on ear headphones
+        </div>
+        <div className={styles.product}>
+          earbuds
+        </div>
+      </section>
     </div>
   );
 }
