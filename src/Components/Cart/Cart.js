e4eb98e6add0309daab52824282c2f9b17af04e8
Sidd Bairan <bairansa@gmail.com>
2022-02-28 04:54:11 +0800

    Finish cart panel styling

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '@mui/material';
 import CartItem from './CartItem';
 import cartStyles from '../../Styles/Cart.module.css';
 
@@ -22,6 +23,34 @@ function Cart() {
             <CartItem />
             <CartItem />
             <CartItem />
+          </div>
+          <div className={cartStyles.totalPriceContainer}>
+            <p>Total: $597</p>
+          </div>
+          <div className={cartStyles.actionButtonsContainer}>
+            <Button
+              variant="contained"
+              color="secondary"
+              style={{
+                width: '16rem',
+                height: '3.5rem',
+                fontSize: '1.3rem',
+                marginBottom: '1rem',
+              }}
+            >
+              CHECKOUT
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              style={{
+                width: '16rem',
+                height: '3.5rem',
+                fontSize: '1.3rem',
+              }}
+            >
+              BROWSE PRODUCTS
+            </Button>
           </div>
         </div>
       </div>
