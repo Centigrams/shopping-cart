96b355862ed18eae8523dc7902595580a7f7394d
Sidd Bairan <bairansa@gmail.com>
2022-03-04 21:33:30 +0800

    Comment cleanup

--- src/features/Cart/Cart.js
+++ src/features/Cart/Cart.js
@@ -3,16 +3,7 @@ import { Button } from '@mui/material';
 import CartItem from './CartItem';
 import cartStyles from '../../Styles/Cart.module.css';
 
-/**
- * Once the cart page is active, the site loses the ability to click on
- * other parts of the website. Maybe use an event listener which closes
- * cart when the dark panel is clicked.
- * */
-// TODO: Cart and cart item specs.
-// Cart should show a shopping cart icon in the middle if empty.
-// Cart panel must show Checkout and Browse Products at the bottom.
-// Cart panel must display total bill directly below the last product in cart.
-// Cart panel must hide scrollbar.
+// TODO: Cart should show a shopping cart icon in the middle if empty.
 function Cart() {
   return (
     <div className={cartStyles.cartBackground}>
